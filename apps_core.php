f881a3254545b8cb6be52e8ef4183dabceb44f87
Keith Brinks <keithbrinks@me.com>
2014-04-23 14:34:41 -0400

    Rename database variable

--- apps_core.php
+++ apps_core.php
@@ -31,7 +31,7 @@ $db_conn;
 try {
 	$db = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME, DB_USER, DB_PASS);
 	if (DEBUG) {
-		$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	$db_conn = true;
 } catch (PDOException $e) {
