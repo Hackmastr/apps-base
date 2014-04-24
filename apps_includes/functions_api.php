a52078b11d451b8c42bb5260984522513119a4bf
Keith Brinks <keithbrinks@me.com>
2014-04-24 12:49:31 -0400

    Changes to API file

--- apps_includes/functions_api.php
+++ apps_includes/functions_api.php
@@ -3,17 +3,18 @@
  * Apps API
  */
 
-$api_actions = array();
+$api_head_actions = array();
+$api_menu_actions = array();
  
 /**
  * Loads actions into head
  */
 function apps_head() {
 
-	global $api_actions;
+	global $api_head_actions;
 	
-	foreach($api_actions as $api_action) {
-		echo $api_action;
+	foreach($api_head_actions as $api_head_action) {
+		echo $api_head_action;
 	}
 	
 }
@@ -23,12 +24,43 @@ function apps_head() {
  */
 function add_stylesheet($src) {
 
-	global $api_actions;
+	global $api_head_actions;
 	
 	// Format our style tag string
 	$tag = '<link rel="stylesheet" type="text/css" href="'. $src .'" />';
 
 	// Add stylesheet to $api_actions
-	array_push($api_actions, $tag);
+	array_push($api_head_actions, $tag);
+
+}
+
+/**
+ * Creates actions
+ */
+function create_actions($actions) {
+
+	global $page, $api_menu_actions;
+
+	// Check if action has been set
+	if (isset($_GET['action']) && array_key_exists($_GET['action'], $actions)) {
+	
+		// Build our actions menu
+		foreach($actions as $action) {
+		
+			array_push($api_menu_actions, '');
+		
+		}
+		
+		// Set the page title
+		$page['title'] = $actions[$_GET['action']]['name'];
+		
+		// Get required file
+		require(APP_SOURCES_PATH .'/'. $actions[$_GET['action']]['file']);
+	
+	} else {
+	
+		$page['error'] = 'Page not found.';
+	
+	}
 
 }
