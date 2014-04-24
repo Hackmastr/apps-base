254d727121aea20046ffc512fd7e3079e9ace5bc
Keith Brinks <keithbrinks@me.com>
2014-04-24 12:54:52 -0400

    Add initial HTML to home template

--- apps_template/template_home.php
+++ apps_template/template_home.php
@@ -1,5 +1,11 @@
 <?php get_header(); ?>
 
-Test
+	<div id="apps_site_content">
+		<div class="apps_site_wrap">
+		
+			<h1><?php get_page_title(); ?></h1>
+		
+		</div>
+	</div>
 
 <?php get_footer(); ?>
