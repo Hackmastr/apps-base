912673e99f1cfb48071e06a3a64dfb1cf46c5573
Keith Brinks <keithbrinks@me.com>
2014-04-24 12:52:16 -0400

    Setup our main manage source file

--- apps_sources/source_manage.php
+++ apps_sources/source_manage.php
@@ -10,8 +10,29 @@ function manage() {
 
 	global $page;
 	
+	// Set the page's title
 	$page['title'] = 'Manage';
-
+	
+	// Setup the actions menu
+	$page['actions'] = array(
+		'locations' => array(
+			'name' => 'Manage Locations',
+			'page' => 'manage',
+			'function' => 'locations',
+			'file' => 'manageLocations.php'
+		),
+		'cells' => array(
+			'name' => 'Manage Cells',
+			'page' => 'manage',
+			'function' => 'cells',
+			'file' => 'manageCells.php'
+		),
+	);
+	create_actions($page['actions']);
+	
+	//call_user_func($page['actions'][$_GET['action']]['function']);
+	
+	// Load the template
 	load_template('manage');
 
 }
