bbd3c63cef4b1912b56380232d08989d19d95d99
Keith Brinks <keithbrinks@me.com>
2014-04-25 11:57:45 -0400

    Add each of our manage sub menu items

--- apps_sources/source_manage.php
+++ apps_sources/source_manage.php
@@ -12,6 +12,16 @@ function manage() {
 	
 	// Build our sub nav
 	$page['sub_nav'] = array(
+		'managelocations' => array(
+			'title' => 'Manage Locations',
+			'function' => 'manage_locations',
+			'source' => 'manage_locations.php'
+		),
+		'managedivisions' => array(
+			'title' => 'Manage Divisions',
+			'function' => 'manage_divisions',
+			'source' => 'manage_divisions.php'
+		),
 		'managecells' => array(
 			'title' => 'Manage Cells',
 			'function' => 'manage_cells',
