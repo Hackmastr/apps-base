89fa2577d432719e2b09fbf781f4d5151e6bd54b
Keith Brinks <keithbrinks@me.com>
2014-04-30 15:38:55 -0400

    Rename to global.css for global styling

--- apps_template/Index.template.php
+++ apps_template/Index.template.php
@@ -16,7 +16,7 @@ function template_header() {
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width">
 		
-		<link rel="stylesheet" type="text/css" href="'. SITE_URL .'/apps_template/style.css" />
+		<link rel="stylesheet" type="text/css" href="'. SITE_URL .'/apps_template/global.css" />
 		
 		<!--[if IE]>
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
