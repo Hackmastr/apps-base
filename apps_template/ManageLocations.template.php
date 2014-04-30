afd800ef46b911b92a6293b47e2a0da177efdd1d
Keith Brinks <keithbrinks@me.com>
2014-04-30 13:26:25 -0400

    Locations form cleanup

--- apps_template/ManageLocations.template.php
+++ apps_template/ManageLocations.template.php
@@ -32,14 +32,14 @@ function template_add() {
 	echo '<div class="apps_content right">
 		<h1>'. get_page_title() .'</h1>
 		
-		<form method="post" action="'. get_page_url() .'">
+		<form class="apps_form" method="post" action="'. get_page_url() .'">
 			<ul>
 				<li>
-					<label for="name">Location Name (e.g. Roosevelt):</label>
+					<label for="name">Location Name</label>
 					<input type="text" id="name" name="name" />
 				</li>
 				<li>
-					<label for="country">Country:</label>
+					<label for="country">Country</label>
 					<select id="country">
 						<option value="us">United States</option>
 						<option value="mexico">Mexico</option>
@@ -51,15 +51,15 @@ function template_add() {
 					<input type="text" id="state" name="state" />
 				</li>
 				<li>
-					<label for="city">City:</label>
+					<label for="city">City</label>
 					<input type="text" id="city" name="city" />
 				</li>
 				<li>
-					<label for="name">Address:</label>
+					<label for="name">Address</label>
 					<input type="text" id="address" name="address" />
 				</li>
 				<li>
-					<label for="name">Zip Code:</label>
+					<label for="name">Zip Code</label>
 					<input type="text" id="zip" name="zip" />
 				</li>
 				<li>
