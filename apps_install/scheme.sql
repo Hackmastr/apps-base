6d200b41499919566c47586caeb00a18f7fcd9b8
Keith Brinks <keithbrinks@me.com>
2014-04-30 14:32:23 -0400

    SQL for app_locations table

---
+++ apps_install/scheme.sql
@@ -0,0 +1,9 @@
+CREATE TABLE app_locations (
+	id bigint(20) unsigned NOT NULL auto_increment,
+	location_name VARCHAR(200) NOT NULL,
+	location_country VARCHAR(200) NOT NULL,
+	location_state VARCHAR(200) NOT NULL,
+	location_street VARCHAR(200) NOT NULL,
+	location_zip VARCHAR(200) NOT NULL,
+	PRIMARY KEY (id)
+);
