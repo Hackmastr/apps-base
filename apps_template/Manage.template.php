10bea0899ec1e658cc412d1788bbea6d54deffdd
Keith Brinks <keithbrinks@me.com>
2014-05-07 14:23:47 -0400

    Move to form to base.functions.php

--- apps_template/Manage.template.php
+++ apps_template/Manage.template.php
@@ -67,45 +67,9 @@ function template_locations_form() {
 			echo $page['the_message'];
 		}
 		
-		echo '<form class="apps_form" method="post" action="'. get_page_url() .'">
-			<ul>
-				<li>
-					<label for="name">Location Name</label>
-					<input type="text" id="name" name="location_name" value="'. $manage->get_value('location_name') .'" />
-				</li>
-				<li>
-					<label for="country">Country</label>
-					<select id="country" name="location_country">
-						<option value="us">United States</option>
-						<option value="mexico">Mexico</option>
-						<option value="hungary">Hungary</option>
-					</select>
-				</li>
-				<li>
-					<label for="state">State/Province:</label>
-					<input type="text" id="state" name="location_state" value="'. $manage->get_value('location_state') .'" />
-				</li>
-				<li>
-					<label for="city">City</label>
-					<input type="text" id="city" name="location_city" value="'. $manage->get_value('location_city') .'" />
-				</li>
-				<li>
-					<label for="address">Street Address</label>
-					<input type="text" id="street" name="location_street" value="'. $manage->get_value('location_street') .'" />
-				</li>
-				<li>
-					<label for="zip">Zip Code</label>
-					<input type="text" id="zip" name="location_zip" value="'. $manage->get_value('location_zip') .'" />
-				</li>
-				<li>
-					<input type="submit" name="submit" value="Submit" />
-					<input type="submit" name="delete" value="Delete" />
-				</li>
-			</ul>
+		get_manage_form($db->area, $page['action']);
 		
-		</form>
-		
-	</div>';
+	echo '</div>';
 
 }
 
