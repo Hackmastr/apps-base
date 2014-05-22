1d0c1db8b58b55ee400d0727bf6b975208651044
Keith Brinks <keithbrinks@me.com>
2014-05-22 13:48:24 -0400

    Remove forward slash from APP_URL

--- apps_core.php
+++ apps_core.php
@@ -12,7 +12,7 @@ require_once('apps_config.php');
 require_once('apps_includes/core.functions.php');
 require_once('apps_includes/db.class.php');
 
-define('APP_URL', SITE_URL .'/'. APP_PATH);
+define('APP_URL', SITE_URL . APP_PATH);
 
 /**
  * Start database connection
