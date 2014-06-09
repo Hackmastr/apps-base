e22214329d72c0cd7e3ead33e3673100368f2807
Keith Brinks <keithbrinks@me.com>
2014-06-09 15:12:30 -0400

    Load index template file in template initializer

--- apps_includes/template.class.php
+++ apps_includes/template.class.php
@@ -4,13 +4,17 @@
  */
 
 class Template {
+
+	var $template_path;
 	
 	/**
 	 * Template initializer
 	 */
-	function __construct() {
+	function __construct($template_path) {
 		
+		$this->template_path = $template_path;
 		
+		require_once($this->template_path .'/Index.template.php');
 		
 	}
 	
