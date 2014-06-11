a4a68f9c898d09ec3b818bfd85235061c92b107b
Keith Brinks <keithbrinks@me.com>
2014-06-11 13:36:15 -0400

    Delete Home source file and move to index.php

--- index.php
+++ index.php
@@ -1,41 +1,11 @@
 <?php
+/**
+ * Main page to Ventura Apps
+ */
 
 // Setup our app environment
-define('APP_PATH', '');
-define('APP_SOURCES_PATH', dirname(__FILE__) .'/apps_sources');
 require_once('apps_core.php');
 $template->set_app_template_path(dirname(__FILE__) .'/apps_template');
-
-try {
-	call_user_func(init());
-} catch (Exception $e) {
-	echo $e->getMessage();
-}
-
-/**
- * Initialize the app
- */
-function init() {
-
-	global $template;
+$template->set_page_title('Home');
 	
-	// Setup our pages array
-	$pages = array(
-		'home' => array('Home', 'Home.php'),
-		'apps' => array('Apps', 'Apps.php'),
-		'manage' => array('Manage', 'Manage.php')
-	);
-
-	// If a page has been requested, and it exists in the $pages array
-	// call corresponding source file and function
-	if (isset($_GET['p']) && array_key_exists($_GET['p'], $pages)) {
-	
-		require_once(APP_SOURCES_PATH .'/'. $pages[$_GET['p']][1]);
-		return $pages[$_GET['p']][0];
-	
-	} else {
-		require_once(APP_SOURCES_PATH .'/'. $pages['home'][1]);
-		return $pages['home'][0];
-	}
-	
-}
+load_template('home');
