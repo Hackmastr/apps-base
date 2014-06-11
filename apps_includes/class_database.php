517cbe64b2b6a534f60809f3a6890830d05c8702
Keith Brinks <keithbrinks@me.com>
2014-06-11 17:39:10 -0400

    Fetch object

--- apps_includes/class_database.php
+++ apps_includes/class_database.php
@@ -35,7 +35,7 @@ class Database {
 
 		try {
 			$this->db = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME, DB_USER, DB_PASS);
-			$this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			$this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 			if (DEBUG) {
 				$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}
