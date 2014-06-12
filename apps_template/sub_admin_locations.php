604687fa0c06d00531ea4f680890c2bd654d50c8
Keith Brinks <keithbrinks@me.com>
2014-06-11 21:51:51 -0400

    Get location data in a table?

--- apps_template/sub_admin_locations.php
+++ apps_template/sub_admin_locations.php
@@ -3,12 +3,19 @@
 // Get our locations class
 $locations = Bootstrap::Locations();
 
-echo '<ol>';
+echo '<table class="apps_list_table">
+	<tr>
+		<th class="app_list_header app_list_title">Title</th>
+	</tr>';
 
 foreach ($locations->get_locations_list() as $location) {
 	
-	echo '<li>'. $location->name .'</li>';
+	echo '<tr>
+		<td class="app_list_data app_list_title">
+			<a href="'. $template->get_option('site_url') .'/admin/index.php?tab=locations&id='. $location->id .'&action=edit">'. $location->name .'</a>
+		</td>
+	</tr>';
 	
 }
 
-echo '</ol>';
+echo '</table>';
