6140286078cd1eea84db6a3d51d058e330ab637c
Keith Brinks <keithbrinks@me.com>
2014-06-26 13:17:51 -0400

    Add apps-dashboard page

--- a/index.php
+++ a/index.php
@@ -1,2 +1,11 @@
 <?php
-/** Die */
+/**
+ * Ventura Apps apps overview page
+ */
+// Setup our app environment
+require_once('../apps-core.php');
+$template->set_app_template_path('/apps_template');
+$template->set_page_title('Apps');
+$template->setParentPage('apps');
+
+load_template('apps-dashboard');
---
+++ apps_template/template-apps-dashboard.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+	<h2><?php echo $template->get_page_title(); ?></h2>
+	
+	<section id="apps_dashboard_links">
+		<ul>
+			<li class="dashboard_link">
+				<a href="<?php $template->get_option('site_url'); ?>/a/monthly">
+					<i class="fa fa-calendar"></i>
+					<h4>Monthly</h4>
+				</a>
+			</li>
+		</ul>
+	</section>
+
+<?php get_footer(); ?>
