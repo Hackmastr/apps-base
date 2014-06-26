5771b07314b01bf00ff65e21214451d0ee1855be
Keith Brinks <keithbrinks@me.com>
2014-06-26 14:37:35 -0400

    Add site footer

--- apps_template/template-footer.php
+++ apps_template/template-footer.php
@@ -1,4 +1,11 @@
 	</main>
+	
+	<footer id="apps_site_footer">
+		Ventura Apps | Core Version: <?php echo APPS_CORE_VERSION; ?>
+		<?php echo (defined('APP_VERSION') ? ' | App Version: '. APP_VERSION : ''); ?>
+		| <a href="mailto:keith.brinks@venturamfg.com">Report A Problem</a>
+	</footer>
+	
 </div>
 
 </body>
