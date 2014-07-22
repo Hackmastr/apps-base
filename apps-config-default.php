e80b5f63aced7e14c53bf2e62d2ca105229d47b8
Keith Brinks <keithbrinks@me.com>
2014-07-22 13:32:52 -0400

    Remove obsolete constants

--- apps-config-default.php
+++ apps-config-default.php
@@ -11,26 +11,6 @@ $site_options = array(
 );
 
 /**
- * Set the site's title
- */
-define('SITE_TITLE', 'Ventura Apps');
-
-/**
- * Set the site's URL
- */
-define('SITE_URL', '//localhost/apps');
-
-/**
- * Set the site's path
- */
-define('SITE_PATH', dirname(__FILE__) .'/');
-
-/**
- * Set the site's template path
- */
-define('SITE_TEMPLATE_PATH', SITE_PATH .'/apps_template');
-
-/**
  * Define database connection settings
  */
 define('DB_HOST', 'localhost');
