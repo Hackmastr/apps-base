e203d17c3b4844f5cb4ad94373ce0c13b3bb2b11
Keith Brinks <keithbrinks@me.com>
2014-08-15 12:39:02 -0400

    Admin templates added?

---
+++ apps_template/template-admin-roles.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+	
+	<div class="row">
+	
+		<div class="col-sm-4 col-md-3 sidebar">
+			<?php get_sidebar(); ?>
+		</div>
+		
+		<div class="col-sm-8 col-md-9">
+		
+			<div class="page-header">
+	
+				<h1><?php echo $template->get_page_title(); ?></h1>
+				
+			</div>
+	
+			<?php get_message(); ?>
+		
+			<?php get_admin_roles_form(); ?>
+			
+		</div>
+	
+	</div>
+
+<?php get_footer(); ?>
