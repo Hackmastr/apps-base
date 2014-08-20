b89157122540e795b412e1358d8dc3d6d376b76b
Keith Brinks <keithbrinks@me.com>
2014-08-20 15:57:13 -0400

    Remove duplicate jQuery include

--- apps_template/template-footer.php
+++ apps_template/template-footer.php
@@ -1,8 +1,5 @@
 </div><!-- /container -->
 
-<!-- jQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
 <!-- Bootstrap JS -->
 <script src="<?php echo $template->get_option('site_url'); ?>/apps_template/js/bootstrap.min.js"></script>
 
