ca4a133f1c38b6ad9cd78209f63e22b50fe97817
Keith Brinks <keithbrinks@me.com>
2015-01-30 11:03:48 -0500

    Move Zeeland lunch schedule to its own page

--- apps_template/template-zeelunch.php
+++ apps_template/template-zeelunch.php
@@ -4,24 +4,11 @@
 		<h1><?php echo $template->page_title; ?></h1>
 	</div>
 	
-	<div class="row dashboard-links">
-		<?php if (Link::getAllLinks()) : foreach(Link::getAllLinks() as $link) : ?>
-		
-			<div class="col-lg-3 col-md-4 col-xs-6">
-				<a class="thumbnail" href="<?php echo $link->getURL(); ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo $link->getDescription(); ?>">
-					<i style="color: <?php echo $link->getBGColor(); ?>" class="fa <?php echo $link->getIconClass(); ?>"></i>
-					<h4><?php echo $link->getName(); ?></h4>
-				</a>
-			</div>			
-		
-		<?php endforeach; else : create_message('info', 'No links exist.', true); endif; ?>
-	</div>
-	
 	<div id="innolunch">
 		
 		<?php echo (get_var('menu') == 'edit' ? '<form class="form-horizontal" method="post" action="'. get_page_url() .'">' : ''); ?>
 		
-		<h2>Lunch Schedule  - Week of <?php echo (get_var('menu') == 'edit' ? '<input name="innolunch_week" type="text" value="'. Innolunch::getWeek() .'" />' : Innolunch::getWeek()); ?> <span class="pull-right"><?php echo (get_var('menu') == 'edit' ? '<a class="btn btn-default btn-xs" href="'. $options['site_url'] .'/index.php#innolunch">Cancel</a> <button class="btn btn-primary btn-xs" type="submit" name="save">Save</button>' : '<a href="'. get_page_url() .'/index.php?menu=edit#innolunch">Edit</a>'); ?></span></h2>
+		<h4>Week of <?php echo (get_var('menu') == 'edit' ? '<input name="innolunch_week" type="text" value="'. Innolunch::getWeek() .'" />' : Innolunch::getWeek()); ?> <span class="pull-right"><?php echo (get_var('menu') == 'edit' ? '<a class="btn btn-default btn-xs" href="'. $options['site_url'] .'/index.php?p=zeelunch#innolunch">Cancel</a> <button class="btn btn-primary btn-xs" type="submit" name="save">Save</button>' : '<a href="'. get_page_url() .'&menu=edit#innolunch">Edit</a>'); ?></span></h4>
 		
 		<table class="table table-bordered table-hover">
 			<thead>
