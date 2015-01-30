6498fd0090322c08eb3c723edf20086f03d80cc9
Keith Brinks <keithbrinks@me.com>
2015-01-30 11:04:06 -0500

    Move Zeeland lunch schedule to its own page

---
+++ apps_template/template-home.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+	<div class="page-header">
+		<h1><?php echo $template->page_title; ?></h1>
+	</div>
+	
+	<div class="row dashboard-links">
+		<?php if (Link::getAllLinks()) : foreach(Link::getAllLinks() as $link) : ?>
+		
+			<div class="col-lg-3 col-md-4 col-xs-6">
+				<a class="thumbnail" href="<?php echo $link->getURL(); ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo $link->getDescription(); ?>">
+					<i style="color: <?php echo $link->getBGColor(); ?>" class="fa <?php echo $link->getIconClass(); ?>"></i>
+					<h4><?php echo $link->getName(); ?></h4>
+				</a>
+			</div>			
+		
+		<?php endforeach; else : create_message('info', 'No links exist.', true); endif; ?>
+	</div>
+	
+	<script type="text/javascript">
+		$(function() {
+			$('.thumbnail').tooltip();
+		});
+	</script>
+
+<?php get_footer(); ?>
