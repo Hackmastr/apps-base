3c938bd2d2b99bb6dd61c71191b8ed269586e2be
Keith Brinks <keithbrinks@me.com>
2015-02-02 15:04:18 -0500

    Update changelog page

--- updates.php
+++ updates.php
@@ -1,32 +1,22 @@
-<?php require 'apps-core.php'; ?>
+<?php
+/**
+* VenturaApps updates page
+*/
 
-<!DOCTYPE html>
-<html>
-	<head>
-		
-		<title>Updates</title>
-		
-		<link href="<?php echo $options['site_url']; ?>/apps_template/css/k.css" rel="stylesheet" media="screen" />
-		<link href="<?php echo $options['site_url']; ?>/apps_template/css/updates.css" rel="stylesheet" media="screen" />
-	
-	</head>
-	<body>
-		
-		<header id="cl-head">
-			<h1>Update Notes</h1>
-			<span class="legend legend-added">Added</span><span class="legend legend-removed">Removed</span><span class="legend legend-fixed">Fixed</span><span class="legend legend-improved">Improved</span>
-		</header>
-		
-		<div id="cl-content">
+####################################################################
+#                        App setup & config                        #
+####################################################################
 
-			<div class="entry">
-				<h2>--</h2>
-				<ul>
-					<li>Initial release.</li>
-				</ul>
-			</div>
-			
-		</div>
-		
-	</body>
-</html>
+require_once 'apps-core.php';
+
+$app = new App();
+$app->name = 'Base';
+$app->prefix = 'apps';
+$app->url = $options['site_url'];
+
+$template = new Template($options, $app);
+
+####################################################################
+
+$template->page_title = 'Update Notes';
+load_template('updates');
