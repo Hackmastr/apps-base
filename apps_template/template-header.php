1bb2b629e61212a88dd52d4e67c587468e0a32d5
Keith Brinks <keithbrinks@me.com>
2015-02-05 10:53:27 -0500

    Add Google Analytics code

--- apps_template/template-header.php
+++ apps_template/template-header.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	
+
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php echo $template->page_title .' - '. $options['site_title']; ?></title>
-		
+
 		<!-- Device support -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
+
 		<!-- jQuery -->
 		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-		
+
 		<!-- jQuery Validate -->
 		<script src="<?php echo $options['site_url']; ?>/apps_template/js/jquery.validate.min.js"></script>
-		
+
 		<!-- Ventura Apps JavaScript -->
 		<script src="<?php echo $options['site_url']; ?>/apps_template/js/ventura-apps.js"></script>
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-		
+
 		<!-- Bootstrap JS -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-		
+
 		<!-- Global CSS -->
 		<link href="<?php echo $options['site_url']; ?>/apps_template/css/apps-global.css" rel="stylesheet" media="screen" />
 
@@ -33,43 +33,56 @@
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		
+
 		<!-- Font Awesome -->
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
 
 		<!-- Apps head -->
-		<?php apps_head(); ?>	
-	
+		<?php apps_head(); ?>
+
 	</head>
 <body<?php if (DEV_MODE) : ?> class="dev"<?php endif; ?>>
-	
+
+<?php if (!DEV_MODE) : ?>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-59288833-1', 'auto');
+	ga('send', 'pageview');
+</script>
+<?php endif; ?>
+
+
 <div id="page-wrap">
-	
+
 	<?php if (DEV_MODE) : ?>
 	<div class="alert alert-danger"><strong>Warning:</strong> Site is currently in dev mode!</div>
 	<?php endif; ?>
-	
+
 	<div class="navbar navbar-inverse apps_main_nav" role="navigation">
 		<div class="container-fluid">
-		
+
 		    <div class="navbar-header">
-				
+
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".main-nav">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-		      
+
 				<a class="navbar-brand" href="<?php echo $options['site_url']; ?>">VenturaApps <sup>Beta</sup></a>
-		    
+
 		    </div>
-			
+
 			<div class="collapse navbar-collapse main-nav">
 				<?php get_nav_menu(); ?>
 			</div>
-			
+
 		</div><!-- /container -->
 	</div><!-- /navbar -->
-	
+
 	<div class="container-fluid main-container">
