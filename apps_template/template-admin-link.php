7844f46b98bb9adee3381f5f5c5035b0a108c878
Keith Brinks <keithbrinks@me.com>
2015-04-16 09:25:39 -0400

    Display link order

--- apps_template/template-admin-link.php
+++ apps_template/template-admin-link.php
@@ -1,26 +1,26 @@
 <?php get_header(); ?>
 
 	<div class="row">
-	
+
 		<div class="col-sm-4 col-md-3 sidebar">
 			<?php echo $template->subnav(); ?>
 		</div>
-		
+
 		<div class="col-sm-8 col-md-9">
-		
+
 			<div class="page-header">
-	
+
 				<h1><?php echo $template->page_title; ?></h1>
-				
+
 			</div>
-			
+
 			<?php get_message(); ?>
-			
+
 			<?php if (is_single()) : ?>
-			
+
 				<?php
 					$link_id = get_var('id');
-			
+
 					$link = ($link_id ? Link::getLink($link_id) : false);
 					$link_name = ($link ? $link->getName() : '');
 					$link_description = ($link ? $link->getDescription() : '');
@@ -29,13 +29,13 @@
 					$link_order = ($link ? $link->getOrder() : '');
 					$link_icon_class = ($link ? $link->getIconClass() : '');
 				?>
-				
+
 				<?php if (get_var('action') != 'add' && !$link) : ?>
-				
+
 					<?php create_message('danger', 'Link does not exist!', true); ?>
-					
+
 				<?php else : ?>
-				
+
 					<form class="form-horizontal" method="post" action="<?php echo get_page_url(); ?>">
 						<div class="form-group">
 							<label class="col-sm-3 control-label" for="name">Link Name</label>
@@ -69,7 +69,7 @@
 						</div>
 						<div class="form-group">
 							<label class="col-sm-3 control-label" for="address">Icon Class</label>
-							<div class="col-sm-9">	
+							<div class="col-sm-9">
 								<input class="form-control" type="text" id="class" name="link_icon_class" value="<?php echo $link_icon_class; ?>" />
 							</div>
 						</div>
@@ -82,38 +82,44 @@
 							</div>
 						</div>
 					</form>
-				
+
 				<?php endif; ?>
-				
+
 			<?php else : ?>
-			
+
 				<a class="btn btn-primary" href="<?php echo $app->url; ?>/admin.php?area=links&action=add">Add New</a>
-		
+
 				<?php if (Link::getAllLinks()) : ?>
-		
+
 					<table class="table">
+						<colgroup>
+							<col width="95%">
+							<col width="5%">
+						</colgroup>
 						<thead>
 							<tr>
 								<th>Title</th>
+								<th>Order</th>
 							</tr>
 						</thead>
 						<tbody>
-							
+
 						<?php foreach (Link::getAllLinks() as $link) : ?>
 							<tr>
 								<td><a href="<?php echo $app->url; ?>/admin.php?area=links&action=view&id=<?php echo $link->getID(); ?>"><?php echo $link->getName(); ?></a></li></td>
+								<td class="text-center"><?php echo $link->getOrder(); ?></td>
 							</tr>
 						<?php endforeach; ?>
-			
+
 						</tbody>
 					</table>
-					
+
 				<?php endif; ?>
-			
+
 			<?php endif; ?>
-			
+
 		</div>
-	
+
 	</div>
 
 <?php get_footer(); ?>
