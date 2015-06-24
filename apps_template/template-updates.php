f31d322cbc3146ce1526f3ee3ed4e7649ffae2cd
Keith Brinks <keithbrinks@me.com>
2015-06-24 09:19:22 -0400

    Update updates

--- apps_template/template-updates.php
+++ apps_template/template-updates.php
@@ -9,7 +9,7 @@
 
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					--/--, 2015
+					June 24th, 2015
 				</div>
 				<div class="panel-body update-notes">
 					<h4>General</h4>
